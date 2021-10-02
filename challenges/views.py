1c24be561172bbacc1c7642665bd4f9cb684a0a3
swillrich5 <swillrich@gmail.com>
2021-10-02 06:43:12 -0500

    adding variables to django template

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": 'Abstain from meat for the entire month',
@@ -42,8 +41,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
+        return render(request, "challenges/challenge.html", { 
+            "text": challenge_text,
+            "month_name": month.capitalize()
+         })
     except:
         return HttpResponseNotFound("<h1>🤬 I don't know what you entered, but it's not supported. 🤬</h1>")
-
-    return HttpResponse(response_data)
